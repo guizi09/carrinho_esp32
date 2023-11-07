c77ed10001567a52ec40b882c5665ae05be05086
guizi09 <guilhermegx2@gmail.com>
2023-11-06 23:58:22 -0300

    Alterações

--- main/main.c
+++ main/main.c
@@ -11,6 +11,11 @@ Author: Guilherme Silva Oliveira / Pedro Augusto Matos Bittencourt Costa
 #include "esp_log.h"
 #include "sdkconfig.h"
 #include "driver/ledc.h" // Biblioteca para PWM
+#include "ps3.h"
+
+//inicialização do retorno do evento do controle
+ps3SetEventCallback(controller_event_cb);
+ps3Init();
 
 
 // Definição de algumas variaveis relevantes
@@ -23,8 +28,18 @@ Author: Guilherme Silva Oliveira / Pedro Augusto Matos Bittencourt Costa
 #define gpioIn1_l298 14
 #define gpioIn2_l298 15
 
-#define gpioPWM_l298 16
-#define gpioPWM_servo 17
+#define gpioPWM_servo 16
+
+
+// Definição das caracteristicaas do PWM
+#define LEDC_TIMER_BIT_NUM 10
+#define LEDC_TIMER_FREQ_HZ 1000000
+#define LEDC_CHANNEL_0 0
+#define LEDC_CHANNEL_1 1
+#define LEDC_CLK_Source_1 RC_FAST_CLK // clock de 8MHz que tem maior precisao
+#define LEDC_hpoint 15
+
+#define LEDC_SPEED_MODE LEDC_HIGH_SPEED_MODE  // 
 
 // Função para inicializar o esp
 void initialize();
@@ -44,20 +59,27 @@ void app_main(void)
 
     initialize();
     
-    // Função para ler depurar a leitura do nível da porta lógica
-    int level;
+
+    char PWM_servo_duty= 0;     //duty cycle do servo (TEM QUE SER ENTRE 5% E 10%)
+    char PWM_motor_duty= 0;     //duty cycle do motor
+    char driving_mode= 0;       // 0=parado, 1= pra frente, 2= pra tras
+    char brake= false;          // ta freiando?
 
     for(;;)
     {
-       receiver_remote_control();
-    }
+        receiver_remote_control();
+        
 
+        ledc_set_duty_and_update(/INSERIR SPEEDMODE/,LEDC_CHANNEL,/INSERIR VAR COM DUTY CYCLE/,LEDC_hpoint);
+    }
 
 }
 
 
 void initialize()
 {
+    printf("\n----------------------- Incialização dos parametros-----------------------\n");
+
     // Comfiguração das portas gpio
     gpio_config_t io_config_gpio;
 
@@ -78,6 +100,65 @@ void initialize()
     //Definição do valor lógico inicial das portas
     gpio_set_level(gpioIn1_l298, low);
     gpio_set_level(gpioIn2_l298, low);
+
+    //Configuração das portas de PWM
+    
+    // ************     Configuração do LEDC CANAL 0
+    ledc_timer_config_t ledc_timer = 
+    {
+        .speed_mode = LEDC_SPEED_MODE, 
+        .duty_resolution = 4,               //numero de bits que representam duty cycle, pra nosso projeto ta suficiente 16 valores de duty cycle
+        .bit_num = LEDC_TIMER_BIT_NUM,
+        .timer_num = 0,
+        .freq_hz = LEDC_TIMER_FREQ_HZ,
+        .clk_cfg = LEDC_CLK_Source_1,
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    ledc_channel_config_t ledc_channel_0 = 
+    {
+        .gpio_num = gpioPWM_l298_1, // Número do pino GPIO do motor
+        .speed_mode = LEDC_SPEED_MODE,
+        .channel = LEDC_CHANNEL_0,
+        .timer_sel = 0,
+        .duty = 0,
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    // ************     Configuração do LEDC CANAL 1
+
+    ledc_timer_config_t ledc_timer = 
+    {
+        .speed_mode = LEDC_SPEED_MODE, 
+        .duty_resolution = 4,               //numero de bits que representam duty cycle, pra nosso projeto ta suficiente 16 valores de duty cycle
+        .bit_num = LEDC_TIMER_BIT_NUM,
+        .timer_num = 0,
+        .freq_hz = LEDC_TIMER_FREQ_HZ,
+        .clk_cfg = LEDC_CLK_Source_1,
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    ledc_channel_config_t ledc_channel_1 = 
+    {
+        .gpio_num = gpioPWM_l298_2, // Número do pino GPIO do motor
+        .speed_mode = LEDC_SPEED_MODE,
+        .channel = LEDC_CHANNEL_1,
+        .timer_sel = 0,
+        .duty = 0,
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    ledc_channel_config_t ledc_channel = 
+    {
+        .channel = LEDC_CHANNEL,
+        .duty = 0,
+        .gpio_num = gpioPWM_servo, // Número do pino GPIO do servo
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_sel = LEDC_TIMER_0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    printf("\n----------------------- Incialização dos parametros -----------------------\n");
 }
 
 void control_wheel(int In1_l298, int In2_l298, float PWM_l298, float PWM_servo)
@@ -113,6 +194,37 @@ void control_wheel(int In1_l298, int In2_l298, float PWM_l298, float PWM_servo)
 
 void receiver_remote_control()
 {
-    printf("Função de recepção ainda em andamento");
+    while (!ps3IsConnected())
+    {
+        // Prevent the Task Watchdog from triggering
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+
+    controller_event_cb()
+
+
 }
 
+void controller_event_cb( ps3_t ps3, ps3_event_t event )
+{
+    if ( ps3.status.battery >= ps3_status_battery_high )
+    print("The controller still has sufficient battery charge");
+
+    if ( ps3.status.charging )
+    print("Controller is charging");
+
+    if ( ps3.button.triangle )
+    print("Currently pressing the trangle button");
+
+    if ( ps3.analog.stick.lx < 0 )
+    print("Currently pulling analog stick to the left");
+
+    if ( event.button_down.cross )
+    print("The user started pressing the X button");
+
+    if ( event.button_up.cross )
+    print("The user released the X button");
+
+    if ( event.analog_changed.stick.lx )
+    print("The user has moved the left stick sideways");
+}
